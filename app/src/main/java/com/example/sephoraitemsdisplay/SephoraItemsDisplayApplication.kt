c09ef5fe048a9ad730170f5feceb99d6338bd5eb
Alexandre Gay <alexandregay248@gmail.com>
2023-09-16 17:06:07 +0200

    Add hilt to the project

---
+++ app/src/main/java/com/example/sephoraitemsdisplay/SephoraItemsDisplayApplication.kt
@@ -0,0 +1,7 @@
+package com.example.sephoraitemsdisplay
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SephoraItemsDisplayApplication : Application()
--- app/src/main/java/com/example/sephoraitemsdisplay/features/main/MainActivity.kt
+++ app/src/main/java/com/example/sephoraitemsdisplay/features/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.sephoraitemsdisplay
+package com.example.sephoraitemsdisplay.features.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sephoraitemsdisplay.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
